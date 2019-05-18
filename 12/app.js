5f8b4bf3601ba29ab6b3bd1ec9e60762a2732f6a
Stas Bashilov <stasbashilov@gmail.com>
2019-05-18 15:13:35 +0300

    F12 add lesson

---
+++ 12/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class EventListeners {
+    constructor(debugMessage = 'test') {
+        this.debugMessage = debugMessage;
+    }
+
+    onCalc(event){
+        // calcForm.removeEventListener('submit',  onCalc);
+        event.preventDefault();
+        const aInput = event.target.querySelector('input[name="a"]');
+        const a = parseFloat(aInput.value);
+        const bInput = event.target.querySelector('input[name="b"]');
+        const b = parseFloat(bInput.value);
+        const operationSelect = event.target.querySelector('select[name="operation"]');
+        const operation = parseFloat(operationSelect.value);
+        const result = eval(`${a} ${operation} ${b}`);
+        document.getElementById('result').innerHTML = `<strong>Result:</strong>${result}`;
+        // console.log(this.debugMessage, eval(`${a} ${operation} ${b}`));
+    }
+
+}
+
+const calcForm = document.getElementById("calc");
+const listeners = new EventListeners('calcForm');
+
+calcForm.addEventListener('submit',  listeners.onCalc.bind(listeners));
+
+// calcForm.removeEventListener('submit',  onCalc);
+
+
+
+
+
+
